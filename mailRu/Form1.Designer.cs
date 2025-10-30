f5f6e5e0d26efb46b8dc63ce35c0f07f36e091b6
2022 <ivan.parysov@mail.ru>
2025-10-31 02:54:03 +0300

    New Feature's.

--- mailRu/Form1.Designer.cs
+++ mailRu/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.left_side = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.console = new System.Windows.Forms.RichTextBox();
             this.request = new System.Windows.Forms.RichTextBox();
             this.buttosList = new System.Windows.Forms.ListBox();
             this.right_side = new System.Windows.Forms.Panel();
@@ -38,11 +42,6 @@
             this.save = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.sqlDataAdapter1 = new Microsoft.Data.SqlClient.SqlDataAdapter();
-            this.console = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.left_side.SuspendLayout();
             this.right_side.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -63,27 +62,74 @@
             this.left_side.Size = new System.Drawing.Size(202, 637);
             this.left_side.TabIndex = 0;
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(196, 24);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Консоль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Прямой запрос SQL";
+            // 
+            // console
+            // 
+            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.console.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.console.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.console.Location = new System.Drawing.Point(3, 462);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(196, 169);
+            this.console.TabIndex = 7;
+            this.console.Text = "";
+            // 
             // request
             // 
             this.request.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.request.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.request.EnableAutoDragDrop = true;
-            this.request.Location = new System.Drawing.Point(6, 403);
+            this.request.Location = new System.Drawing.Point(3, 403);
             this.request.Name = "request";
-            this.request.Size = new System.Drawing.Size(193, 34);
+            this.request.Size = new System.Drawing.Size(196, 34);
             this.request.TabIndex = 3;
             this.request.Text = "";
+            this.request.TextChanged += new System.EventHandler(this.request_TextChanged);
+            this.request.KeyUp += new System.Windows.Forms.KeyEventHandler(this.request_KeyUp);
             // 
             // buttosList
             // 
+            this.buttosList.BackColor = System.Drawing.SystemColors.Control;
+            this.buttosList.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.buttosList.FormattingEnabled = true;
             this.buttosList.Items.AddRange(new object[] {
             "main",
             "printing_house",
             "newspaper"});
-            this.buttosList.Location = new System.Drawing.Point(6, 29);
+            this.buttosList.Location = new System.Drawing.Point(3, 42);
             this.buttosList.Name = "buttosList";
-            this.buttosList.Size = new System.Drawing.Size(193, 355);
+            this.buttosList.Size = new System.Drawing.Size(196, 338);
             this.buttosList.TabIndex = 1;
             this.buttosList.SelectedIndexChanged += new System.EventHandler(this.buttosList_SelectedIndexChanged);
             // 
@@ -92,7 +138,6 @@
             this.right_side.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.right_side.Controls.Add(this.richTextBox1);
             this.right_side.Controls.Add(this.update);
             this.right_side.Controls.Add(this.commandLine);
             this.right_side.Controls.Add(this.remove);
@@ -121,7 +166,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.commandLine.BackColor = System.Drawing.SystemColors.Menu;
             this.commandLine.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.commandLine.Location = new System.Drawing.Point(13, 3);
+            this.commandLine.Location = new System.Drawing.Point(13, 12);
             this.commandLine.Name = "commandLine";
             this.commandLine.Size = new System.Drawing.Size(629, 24);
             this.commandLine.TabIndex = 4;
@@ -158,67 +203,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonShadow;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 29);
+            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(629, 485);
+            this.dataGridView1.Size = new System.Drawing.Size(629, 472);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // console
-            // 
-            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.console.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.console.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.console.Location = new System.Drawing.Point(6, 462);
-            this.console.Name = "console";
-            this.console.Size = new System.Drawing.Size(193, 169);
-            this.console.TabIndex = 7;
-            this.console.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 387);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Прямой запрос SQL";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 446);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Консоль";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(-202, 3);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(193, 24);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox2.BackColor = System.Drawing.SystemColors.Menu;
-            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox2.Location = new System.Drawing.Point(6, 3);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(193, 24);
-            this.richTextBox2.TabIndex = 7;
-            this.richTextBox2.Text = "";
             // 
             // Form1
             // 
@@ -254,7 +245,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- mailRu/Form1.cs
+++ mailRu/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 
@@ -14,27 +15,37 @@ namespace mailRu
 
         public Form1()
         {
+            InitializeComponent();
+
+            console.Text += $"[{DateTime.Now}] Инициализация Form1...\r\n";
+
             sqlConnector = new SqlConnector();
             sqlConnector.Open();
+            console.Text += $"[{DateTime.Now}] Подключение к базе открыто.\r\n";
+
             sqlConnector.Push("USE Mail;", null);
+            console.Text += $"[{DateTime.Now}] Используется база Mail.\r\n";
 
             sqlService = new SqlConnectorService(sqlConnector);
 
-            InitializeComponent();
-
             this.dataGridView1.DataError += new DataGridViewDataErrorEventHandler(dataGridView1_DataError);
+            console.Text += $"[{DateTime.Now}] Подписка на событие DataError завершена.\r\n";
         }
 
         private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
         {
+            console.Text += $"[{DateTime.Now}] Ошибка DataGridView: {e.Exception.Message}\r\n";
+
             MessageBox.Show("Произошла ошибка при обработке данных: " + e.Exception.Message);
 
             if (e.Context == DataGridViewDataErrorContexts.Commit)
             {
+                console.Text += $"[{DateTime.Now}] Ошибка контекста Commit.\r\n";
                 MessageBox.Show("Ошибка при сохранении данных.");
             }
             else if (e.Context == DataGridViewDataErrorContexts.Parsing)
             {
+                console.Text += $"[{DateTime.Now}] Ошибка контекста Parsing.\r\n";
                 MessageBox.Show("Ошибка разбора данных.");
             }
 
@@ -43,98 +54,134 @@ namespace mailRu
 
         private void buttosList_SelectedIndexChanged(object sender, EventArgs e)
         {
+            console.Text += $"[{DateTime.Now}] Выбран пункт списка: {buttosList.SelectedItem}\r\n";
             UpdateDataGridView();
         }
 
         private void UpdateDataGridView()
         {
+            console.Text += $"[{DateTime.Now}] Обновляется DataGridView для индекса: {buttosList.SelectedIndex}\r\n";
+
             switch (buttosList.SelectedIndex)
             {
                 case 0:
                     updateView("SELECT m.id, m.addr, m.number_newspaper, m.newspaper_id, n.title AS newspaper_title\r\nFROM main m\r\nLEFT JOIN newspaper n ON m.newspaper_id = n.id;\r\n",
                         dataGridView1
                     );
+                    console.Text += $"[{DateTime.Now}] Загружены данные почты.\r\n";
                     break;
                 case 1:
-                    updateView("SELECT * FROM printing_house;",
-                        dataGridView1
-                    );
+                    updateView("SELECT * FROM printing_house;", dataGridView1);
+                    console.Text += $"[{DateTime.Now}] Загружены данные типографии.\r\n";
                     break;
                 case 2:
                     updateView("SELECT n.id, n.title, n.edition_code, n.price, n.full_name, n.printing_house, ph.addr AS printing_house_addr, n.number\r\nFROM newspaper n\r\nLEFT JOIN printing_house ph ON n.printing_house = ph.id;",
                         dataGridView1
                     );
+                    console.Text += $"[{DateTime.Now}] Загружены данные газет.\r\n";
+                    break;
+                default:
+                    console.Text += $"[{DateTime.Now}] Неизвестный индекс в списке.\r\n";
                     break;
             }
         }
 
         private void updateView(string sql, DataGridView dataGridView1)
         {
-            dataGridView1.DataSource = sqlConnector.Fill(sql);
+            console.Text += $"[{DateTime.Now}] Выполняется SQL-запрос: {sql.Trim()}\r\n";
+            try
+            {
+                dataGridView1.DataSource = sqlConnector.Fill(sql);
+                console.Text += $"[{DateTime.Now}] Данные успешно загружены и применены к DataGridView.\r\n";
+            }
+            catch (Exception ex)
+            {
+                console.Text += $"[{DateTime.Now}] Ошибка при загрузке данных: {ex.Message}\r\n";
+                MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+            }
         }
 
         private void save_Click(object sender, EventArgs e1)
         {
             var table = (DataTable)dataGridView1.DataSource;
-            var changedRows = table.GetChanges();
+            var changedRows = table?.GetChanges();
 
             if (changedRows != null)
             {
+                console.Text += $"[{DateTime.Now}] Обнаружено изменений: {changedRows.Rows.Count}\r\n";
+
                 foreach (DataRow row in changedRows.Rows)
                 {
                     if (row.RowState == DataRowState.Added)
                     {
-                        console.Text += "ed";
+                        console.Text += $"[{DateTime.Now}] Добавляется новая запись.\r\n";
 
                         switch (buttosList.SelectedIndex)
                         {
                             case 0:
                                 sqlService.AddNewMail(row);
+                                console.Text += $"[{DateTime.Now}] Вставлена новая почтовая запись.\r\n";
                                 break;
                             case 1:
                                 sqlService.AddNewPrintingHouse(row);
+                                console.Text += $"[{DateTime.Now}] Вставлена новая типография.\r\n";
                                 break;
                             case 2:
                                 sqlService.AddNewNewspaper(row);
+                                console.Text += $"[{DateTime.Now}] Вставлена новая газета.\r\n";
+                                break;
+                            default:
+                                console.Text += $"[{DateTime.Now}] Неизвестный тип данных для вставки.\r\n";
                                 break;
                         }
                     }
                 }
+            }
+            else
+            {
+                console.Text += $"[{DateTime.Now}] Изменений для сохранения не обнаружено.\r\n";
             }
         }
 
         private void commandLine_TextChanged(object sender, EventArgs e)
         {
             Exception error;
+            console.Text += $"[{DateTime.Now}] Выполняется поиск с ключевым словом: '{commandLine.Text}'.\r\n";
 
             var resultTable = sqlService.SearchAndFillDataGridView(buttosList.SelectedItem?.ToString(), commandLine.Text, out error);
 
             if (error != null)
             {
+                console.Text += $"[{DateTime.Now}] Ошибка при поиске: {error.Message}\r\n";
                 MessageBox.Show("Ошибка при поиске: " + error.Message);
             }
             else
             {
                 dataGridView1.DataSource = resultTable;
+                console.Text += $"[{DateTime.Now}] Поиск выполнен успешно, данные обновлены.\r\n";
             }
-
         }
 
         private void SaveChanges(DataTable combinedTable, Action<SqlConnection, SqlTransaction, DataRow> updateAction)
         {
             try
             {
+                console.Text += $"[{DateTime.Now}] Начинается сохранение изменений.\r\n";
                 sqlService.SaveChanges(combinedTable, updateAction);
+                console.Text += $"[{DateTime.Now}] Изменения успешно сохранены.\r\n";
                 MessageBox.Show("Изменения сохранены.");
             }
             catch (Exception ex)
             {
+                console.Text += $"[{DateTime.Now}] Ошибка при сохранении изменений: {ex.Message}\r\n";
                 MessageBox.Show("Ошибка при сохранении: " + ex.Message);
             }
         }
 
         private void update_Click(object sender, EventArgs e)
         {
+            console.Text += $"[{DateTime.Now}] Обновление записи с индексом: {buttosList.SelectedIndex}\r\n";
+
             switch (buttosList.SelectedIndex)
             {
                 case 0:
@@ -146,17 +193,23 @@ namespace mailRu
                 case 2:
                     SaveChanges((DataTable)dataGridView1.DataSource, sqlService.UpdateNewspaper);
                     break;
+                default:
+                    console.Text += $"[{DateTime.Now}] Неизвестный индекс для обновления.\r\n";
+                    break;
             }
         }
 
         private int GetSelectedIdFromDataGrid()
         {
+            console.Text += $"[{DateTime.Now}] Определение выбранного ID в DataGridView.\r\n";
+
             if (dataGridView1.SelectedRows.Count > 0)
             {
                 DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
                 object cellValue = selectedRow.Cells["id"].Value;
                 if (cellValue != null && int.TryParse(cellValue.ToString(), out int id))
                 {
+                    console.Text += $"[{DateTime.Now}] Выбранный ID: {id}.\r\n";
                     return id;
                 }
             }
@@ -165,65 +218,114 @@ namespace mailRu
                 object cellValue = dataGridView1.CurrentRow.Cells["id"].Value;
                 if (cellValue != null && int.TryParse(cellValue.ToString(), out int id))
                 {
+                    console.Text += $"[{DateTime.Now}] Текущий ID: {id}.\r\n";
                     return id;
                 }
             }
 
-            throw new InvalidOperationException("Не выбрана строка или столбец 'id' отсутствует или содержит неверное значение");
+            string errorMessage = "Не выбрана строка или столбец 'id' отсутствует или содержит неверное значение";
+            console.Text += $"[{DateTime.Now}] Ошибка: {errorMessage}\r\n";
+            throw new InvalidOperationException(errorMessage);
         }
 
         private void remove_Click(object sender, EventArgs e)
         {
+            console.Text += $"[{DateTime.Now}] Начало удаления записи с индексом: {buttosList.SelectedIndex}\r\n";
+
             switch (buttosList.SelectedIndex)
             {
-                // Почта (main)
                 case 0:
                     {
-                        var idToDelete = GetSelectedIdFromDataGrid(); // Ваш метод получить выбранный ID из dataGridView1
+                        var idToDelete = GetSelectedIdFromDataGrid();
+                        console.Text += $"[{DateTime.Now}] Удаляется запись из main с ID: {idToDelete}\r\n";
 
-                        // Просто удаляем строку из main по id
                         sqlConnector.Push("DELETE FROM main WHERE id = @id", cmd =>
                         {
                             cmd.Parameters.AddWithValue("@id", idToDelete);
                         });
+
+                        console.Text += $"[{DateTime.Now}] Удаление записи из main завершено.\r\n";
                     }
                     break;
 
-                // Типография (printing_house)
                 case 1:
                     {
                         var idToDelete = GetSelectedIdFromDataGrid();
+                        console.Text += $"[{DateTime.Now}] Удаление связанных записей с типографией ID: {idToDelete}\r\n";
 
-                        // Удаляем сначала все main, связанные с газетами из данной типографии
                         sqlConnector.Push(@"
-                    DELETE main 
-                    WHERE newspaper_id IN 
-                    (SELECT id FROM newspaper WHERE printing_house = @id);
-                    DELETE newspaper WHERE printing_house = @id;
-                    DELETE printing_house WHERE id = @id;", cmd =>
+                        DELETE main 
+                        WHERE newspaper_id IN 
+                        (SELECT id FROM newspaper WHERE printing_house = @id);
+                        DELETE newspaper WHERE printing_house = @id;
+                        DELETE printing_house WHERE id = @id;", cmd =>
                         {
                             cmd.Parameters.AddWithValue("@id", idToDelete);
                         });
+
+                        console.Text += $"[{DateTime.Now}] Удаление типографии и связанных записей завершено.\r\n";
                     }
                     break;
 
-                // Газетка (newspaper)
                 case 2:
                     {
                         var idToDelete = GetSelectedIdFromDataGrid();
+                        console.Text += $"[{DateTime.Now}] Удаление газеты с ID: {idToDelete} и связанных почтовых записей.\r\n";
 
-                        // Удаляем сначала все main, связанные с газетой, потом саму газету
                         sqlConnector.Push(@"
-                    DELETE FROM main WHERE newspaper_id = @id;
-                    DELETE FROM newspaper WHERE id = @id;", cmd =>
+                        DELETE FROM main WHERE newspaper_id = @id;
+                        DELETE FROM newspaper WHERE id = @id;", cmd =>
                         {
                             cmd.Parameters.AddWithValue("@id", idToDelete);
                         });
+
+                        console.Text += $"[{DateTime.Now}] Удаление газеты и связанных записей завершено.\r\n";
                     }
+                    break;
+
+                default:
+                    console.Text += $"[{DateTime.Now}] Неизвестный индекс для удаления.\r\n";
                     break;
             }
 
             UpdateDataGridView();
+            console.Text += $"[{DateTime.Now}] Обновление DataGridView после удаления.\r\n";
+        }
+
+        private void request_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void request_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                console.Text += $"[{DateTime.Now}] Выполняется SQL-запрос:\r\n{request.Text}\r\n";
+
+                sqlConnector.Read(request.Text, reader =>
+                {
+                    try
+                    {
+                        var rowValues = new List<string>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            string columnName = reader.GetName(i);
+                            object value = reader.IsDBNull(i) ? "NULL" : reader.GetValue(i);
+                            rowValues.Add($"{columnName}={value}");
+                        }
+
+                        string rowText = string.Join(", ", rowValues);
+                        console.Text += $"[{DateTime.Now}] Строка результата: {rowText}\r\n";
+                    }
+                    catch (Exception ex)
+                    {
+                        console.Text += $"[{DateTime.Now}] Ошибка при обработке результата: {ex.Message}\r\n";
+                    }
+                });
+
+                console.Text += $"[{DateTime.Now}] Выполнение запроса завершено.\r\n";
+            }
         }
     }
 }
